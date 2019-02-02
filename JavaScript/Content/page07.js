418361d322c6daeb6106f9664f84c7fb1581a9a8
Pavlov83 <codedriverun@gmail.com>
2019-02-02 13:11:54 +0200

    functions with parameters in JS

---
+++ JavaScript/Content/page07.js
@@ -0,0 +1,15 @@
+//declaring functions
+
+function func_name([param1,param2 ...]){
+	expression1;
+	expression2;
+
+
+
+
+function showMessage(){
+
+	document.write('<h1>Message Text<h1>');
+}
+
+showMessage();
